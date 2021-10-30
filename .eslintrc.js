8a8f9198ae48d610911efbce60b86ce055068e57
Matthew Aberham <matthew.aberham@gmail.com>
2021-10-30 17:25:12 -0400

    Remove prettier linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'prettier/prettier': 'off',
         'space-before-function-paren': 'off',
         semi: 'off',
         indent: ['error', 4],
